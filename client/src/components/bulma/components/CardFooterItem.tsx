7e584ec7b73fc047af45ee8e2fd463229c998b47
Sebastian Fuchs <12152164+foxable@users.noreply.github.com>
2019-06-16 11:38:23 +0200

    Render card footer item with href attr as link

--- client/src/components/bulma/components/CardFooterItem.tsx
+++ client/src/components/bulma/components/CardFooterItem.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import * as classNames from "classnames";
 
-export const CardFooterItem: React.FunctionComponent<React.HTMLProps<HTMLDivElement>> = ({ className, ...props }: React.HTMLProps<HTMLDivElement>) => {
+export type CardFooterItemProps = React.HTMLProps<HTMLDivElement & HTMLAnchorElement>;
+
+export const CardFooterItem: React.FunctionComponent<CardFooterItemProps> = ({ className, ...props }: CardFooterItemProps) => {
     const cardFooterItemClass = classNames(
         "card-footer-item",
         className
     );
+
+    if (props.href)
+    {
+        return <a className={cardFooterItemClass} {...props}></a>;
+    }
 
     return <div className={cardFooterItemClass} {...props}></div>;
 };
